bff5d9a2b818a566dc22d67606a8b09f7d54d398
Jorge Sazo <jorge888davila@gmail.com>
2022-04-20 12:02:15 -0600

    Añadida la funcion para leer el archivo por caracter

--- PLANTILLA C.c
+++ PLANTILLA C.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-void main(){
+//Funcion para leer el archivo de entrada
+void leerArchivo(char const *ruta){
 	
-	printf("Te quiero Rick");
+    FILE * file;
+    int data;
+ 
+    if( access(ruta, F_OK) != -1){
+    	
+	    file = fopen(ruta, "r");
+	    while (1) {
+	        data = fgetc(file);
+	        if (feof(file))
+	            break;
+	        printf("%c", data);
+	    }
+	    fclose(file);
+	    
+    }else{
+    	printf("El archivo no existe en esta ruta");
+    }
+}
+
+//funcion main de ejecucion
+int main()
+{
+ 	char ruta[256];
+	printf("Ingrese la ruta del archivo: \n");
+	scanf("%s", ruta); 
 	
+	leerArchivo(ruta);
 }
